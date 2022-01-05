77106def6eedcf739750a26c9e98398912734f13
David Betteridge <davidabetteridge@gmail.com>
2022-01-05 22:05:25 +0000

    Day 8 - Part 1

---
+++ Day08/day8.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+def read_file() -> List[str]:
+  with open('Day08/data.txt') as f:
+    return [line.strip() for line in f.readlines()]
+
+
+
+def in_memory_size(line: str) -> int:
+  base = line[1:-1]
+  number_of_quotes = (len(base) - len(base.replace('\\"', ""))) / 2
+  number_of_backslashes = (len(base) - len(base.replace('\\\\', ""))) / 2
+
+  number_of_hex = 0
+  for i in range(len(base)-3):
+    if base[i] == "\\" and base[i+1] == "x" and base[i+2] in "0123456789abcdef" and base[i+3] in "0123456789abcdef":
+      number_of_hex +=1
+
+  return len(base) - number_of_quotes - number_of_backslashes - (number_of_hex * 3)
+
+
+lines = read_file()
+print(sum([len(line) - in_memory_size(line) for line in lines]))
+  
+#1333
