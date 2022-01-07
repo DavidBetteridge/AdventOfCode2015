a78a8ff271966e26462b8d830f8d4a17f6b764ca
David Betteridge <davidabetteridge@gmail.com>
2022-01-07 18:01:29 +0000

    Part 2

--- Day16/day16.py
+++ Day16/day16.py
@@ -1,3 +1,4 @@
+import operator
 from typing import Dict, List, Tuple
 
 def parse_line(line: str) -> Tuple[int, Dict[str, int]]:
@@ -17,18 +18,21 @@ def read_file() -> List[Tuple[int, Dict[str, int]]]:
   with open('Day16/data.txt') as f:
     return [parse_line(line.strip()) for line in f.readlines()]
 
-def filter_sues(current: List[Tuple[int, Dict[str, int]]], field_name, value) -> List[Tuple[int, Dict[str, int]]]:
-  return [sue for sue in current if field_name not in sue[1] or sue[1][field_name] == value ]
+def filter_sues(current: List[Tuple[int, Dict[str, int]]],
+                field_name: str, value: int, operator_fn: callable) \
+                                              -> List[Tuple[int, Dict[str, int]]]:
+  return [sue for sue in current
+          if field_name not in sue[1] or operator_fn(sue[1][field_name], value) ]
 
 sues = read_file()
-sues = filter_sues(sues, "children", 3)
-sues = filter_sues(sues, "cats", 7)
-sues = filter_sues(sues, "samoyeds", 2)
-sues = filter_sues(sues, "pomeranians", 3)
-sues = filter_sues(sues, "akitas", 0)
-sues = filter_sues(sues, "vizslas", 0)
-sues = filter_sues(sues, "goldfish", 5)
-sues = filter_sues(sues, "trees", 3)
-sues = filter_sues(sues, "cars", 2)
-sues = filter_sues(sues, "perfumes", 1)
+sues = filter_sues(sues, "children", 3, operator.eq)
+sues = filter_sues(sues, "cats", 7, operator.gt)
+sues = filter_sues(sues, "samoyeds", 2, operator.eq)
+sues = filter_sues(sues, "pomeranians", 3, operator.lt)
+sues = filter_sues(sues, "akitas", 0, operator.eq)
+sues = filter_sues(sues, "vizslas", 0, operator.eq)
+sues = filter_sues(sues, "goldfish", 5, operator.lt)
+sues = filter_sues(sues, "trees", 3, operator.gt)
+sues = filter_sues(sues, "cars", 2, operator.eq)
+sues = filter_sues(sues, "perfumes", 1, operator.eq)
 print(sues)
