7a9138a078f796f8aeb1f32fc717a6f9431ffeb4
David Betteridge <davidabetteridge@gmail.com>
2022-01-07 18:34:51 +0000

    Day17 - Part 2

--- Day17/day17.py
+++ Day17/day17.py
@@ -7,9 +7,18 @@ def read_file() -> List[int]:
 
 containers = read_file()    
 
-total = sum( [  len([c for c in combinations(containers, r=size) if sum(c) == 150])
-             for size in range(2, len(containers)) ] )
+total = sum( [  len([c for c in combinations(containers, r=size) if sum(c) == 150]) for size in range(2, len(containers)) ] )
 print(total)  #1304
+
+
+for size in range(2, len(containers)):
+  total = len([c for c in combinations(containers, r=size) if sum(c) == 150])
+  if total != 0:
+    print(total)   #18
+    break
+
+
+
 
 
 
