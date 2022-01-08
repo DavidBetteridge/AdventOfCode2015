9b40fa78008523f6103dcf0ede9c2f9f1463f9f9
David Betteridge <davidabetteridge@gmail.com>
2022-01-08 00:23:21 +0000

    Day 19 - Part 2

--- Day19/day19.py
+++ Day19/day19.py
@@ -3,7 +3,7 @@ from typing import DefaultDict, Tuple
 
 
 def read_file() -> Tuple[dict, str]:
-  with open('Day19/data.txt') as f:
+  with open('C:\Personal\AdventOfCode2015\Day19\data.txt') as f:
     lines = [line.strip() for line in f.readlines()]
     
     replacements = DefaultDict(list)
@@ -14,21 +14,44 @@ def read_file() -> Tuple[dict, str]:
     medicine_molecule = lines[-1]
     return replacements, medicine_molecule
 
-
 replacements, medicine_molecule = read_file()
 
-possible_strings = set()
-i = 0
-while i < len(medicine_molecule):
-  if medicine_molecule[i] in replacements:
-    for replacement in replacements[medicine_molecule[i]]:
-      temp = medicine_molecule[:i] + replacement + medicine_molecule[i+1:]
-      possible_strings.add(temp)
-  elif i+1 < len(medicine_molecule) and medicine_molecule[i:i+2] in replacements:
-    for replacement in replacements[medicine_molecule[i:i+2]]:
-      temp = medicine_molecule[:i] + replacement + medicine_molecule[i+2:]
-      possible_strings.add(temp)      
-  i+=1
+ 
+def part_one():
+  possible_strings = set()
+  i = 0
+  while i < len(medicine_molecule):
+    if medicine_molecule[i] in replacements:
+      for replacement in replacements[medicine_molecule[i]]:
+        temp = medicine_molecule[:i] + replacement + medicine_molecule[i+1:]
+        possible_strings.add(temp)
+    elif i+1 < len(medicine_molecule) and medicine_molecule[i:i+2] in replacements:
+      for replacement in replacements[medicine_molecule[i:i+2]]:
+        temp = medicine_molecule[:i] + replacement + medicine_molecule[i+2:]
+        possible_strings.add(temp)      
+    i+=1
 
-print(len(possible_strings))      #535
+  print(len(possible_strings))      #535
 
+
+def part_two():
+  reverse_replacements = {rhs:lhs 
+        for lhs in replacements
+        for rhs in replacements[lhs]}
+  lookup = sorted(reverse_replacements, key=lambda s: len(s), reverse=True)
+
+  number_of_replacements = 0
+  molecule = medicine_molecule
+  while molecule != "e":
+    for rhs in lookup:
+      start_index = molecule.find(rhs)
+      if start_index != -1:
+        molecule = molecule[:start_index] + reverse_replacements[rhs] + molecule[start_index+len(rhs):]
+        number_of_replacements+=1
+        break
+
+  print(number_of_replacements)    #212
+
+
+part_one()
+part_two()
