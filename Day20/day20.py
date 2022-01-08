5eac4b9b9d3509003c5a8f17de8a62c698035e84
David Betteridge <davidabetteridge@gmail.com>
2022-01-08 00:47:49 +0000

    Day20 - Part 2

--- Day20/day20.py
+++ Day20/day20.py
@@ -1,10 +1,24 @@
-houses = [0] * 3310000
+def part1():
+  houses = [0] * 3310000
 
-for elf in range(1, 3310000):
-  for house in range(elf, 3310000, elf):
-    houses[house] += elf
+  for elf in range(1, 3310000):
+    for house in range(elf, 3310000, elf):
+      houses[house] += elf
 
-for no,qty in enumerate(houses):
-  if qty >= 3310000:
-    print(no)  #776160
-    break
+  for no,qty in enumerate(houses):
+    if qty >= 3310000:
+      print(no)  #776160
+      break
+
+
+def part2():
+  houses = [0] * 331000000
+
+  for elf in range(1, 3310000):
+    for h in range(1, 51):
+      houses[elf*h] += (elf * 11)
+
+  for no,qty in enumerate(houses):
+    if qty >= 33100000:
+      print(no)  #786240
+      break
